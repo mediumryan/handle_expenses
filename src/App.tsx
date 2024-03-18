4500d75cd562db77eb336604ecc091451ff798b7
Ryan <straightwind0726@gmail.com>
2024-03-18 17:09:13 +0900

    [Update] Create the Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,39 @@ import './CSS/index.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Years from './Pages/Years';
+import { FaBars } from 'react-icons/fa';
 
-const MainWrapper = styled.main``;
+const MainWrapper = styled.main`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    & > h3 {
+        width: 50%;
+        margin: 2rem auto 0.5rem auto;
+        padding: 1rem 5rem;
+        border-radius: 10px;
+        font-size: 1.5rem;
+        letter-spacing: 4px;
+        font-style: italic;
+        font-weight: bold;
+        text-align: center;
+        color: var(--accent-100);
+        background-color: var(--accent-300);
+        box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+    }
+`;
+
+const HeaderToggle = styled(FaBars)`
+    position: absolute;
+    top: 24px;
+    left: 24px;
+`;
 
 function App() {
     return (
         <MainWrapper>
-            <header></header>
+            <HeaderToggle />
+            <h3>🐮처럼 벌고 🐶처럼 쓴다</h3>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/years/:years" element={<Years />} />
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,62 @@
+import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 const PageWrapper = styled.div`
     width: 100%;
     height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const PageTitle = styled.h3`
+    margin: 2rem auto;
+    padding: 1rem 5rem;
+    border-radius: 10px;
+    font-size: 1.5rem;
+    letter-spacing: 4px;
+    font-style: italic;
+    font-weight: bold;
+    color: var(--accent-100);
+    background-color: var(--accent-300);
+    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+`;
+
+const HomeWrapper = styled.div`
+    margin-top: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 75%;
+    height: 400px;
+    a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-basis: 50%;
+        width: 100%;
+        height: 100%;
+        color: var(--accent-100);
+        background-color: var(--accent-300);
+        margin: 1rem;
+        border-radius: 10px;
+        text-decoration: none;
+        font-size: 2rem;
+        transition: 300ms opacity;
+        &:hover {
+            opacity: 0.5;
+        }
+    }
 `;
 
 export default function Home() {
-    return <PageWrapper>Home</PageWrapper>;
+    return (
+        <PageWrapper>
+            <PageTitle>Home</PageTitle>
+            <HomeWrapper>
+                <Link to="/years/2024">2024년</Link>
+                <Link to="/years/2025">2025년</Link>
+            </HomeWrapper>
+        </PageWrapper>
+    );
 }
